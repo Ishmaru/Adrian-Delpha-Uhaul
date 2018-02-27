063f0ef04bf172f003453b75d765ab16448ef72e
Adrian Delpha <adrian.delpha@gmail.com>
2018-02-27 12:19:28 -0700

    working routes

--- js/controllers/posts-controller.js
+++ js/controllers/posts-controller.js
@@ -4,13 +4,14 @@
   angular
     .module('uhaulApp')
     .controller('PostsController', ['$scope', '$http', '$state', '$stateParams', function($scope, $http, $state, $stateParams) {
-      console.log($stateParams.userId);
       $scope.posts = [];
       $scope.grabPosts = function(userId){
+      console.log('http://jsonplaceholder.typicode.com/posts?userId=' + userId.split(':')[1]);
         $http({
           method: 'GET',
-          url: 'http://jsonplaceholder.typicode.com/posts?userId=' + userId
+          url: 'http://jsonplaceholder.typicode.com/posts?userId=' + userId.split(':')[1]
         }).then(function successCallback(res) {
+          console.log(res.data)
             res.data.forEach(function(post){
               $scope.posts.push(post);
             });
